3b72490fb04becf90ed04b9ad4dd4882243ddd1c
Kaibin <diaokaibin@gmail.com>
2020-08-23 19:53:02 +0800

    init

--- app/src/main/java/com/one/roc/MainActivity.java
+++ app/src/main/java/com/one/roc/MainActivity.java
@@ -1,8 +1,16 @@
 package com.one.roc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import java.io.IOException;
+
+import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Cache;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.cache(new Cache(getCacheDir(), 25 * 1024 * 1024));
+        OkHttpClient client = builder.build();
+        Request request = new Request.Builder().url("http://github.com").build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
+
+
     }
 }
