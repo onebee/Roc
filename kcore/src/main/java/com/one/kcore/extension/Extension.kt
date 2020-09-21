53e567a28edc4b027657d478985ebd7c658dd690
Kaibin <diaokaibin@gmail.com>
2020-09-21 21:30:30 +0800

    update

--- kcore/src/main/java/com/one/kcore/extension/Extension.kt
+++ kcore/src/main/java/com/one/kcore/extension/Extension.kt
@@ -1,15 +1,13 @@
 package com.one.kcore.extension
 
-import android.content.Context
+import android.content.res.Resources
+import android.util.TypedValue
 
 /**
  * @author  diaokaibin@gmail.com on 2020/8/29.
  */
 
 
-fun Context.startActivity(){
-
-
-
+fun dp2px(value: Float): Float {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, Resources.getSystem().displayMetrics)
 }
-
--- kcore/src/main/java/com/one/kcore/test/test.kt
+++
@@ -1,5 +0,0 @@
-package com.one.kcore.test
-
-/**
- * @author  diaokaibin@gmail.com on 2020/8/23.
- */
