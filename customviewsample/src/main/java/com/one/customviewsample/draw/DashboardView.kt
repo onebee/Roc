b2013386340a7367ae260ed7335267a6637a0e22
Kaibin <diaokaibin@gmail.com>
2020-09-28 11:41:35 +0800

    update

--- customviewsample/src/main/java/com/one/customviewsample/draw/DashboardView.kt
+++ customviewsample/src/main/java/com/one/customviewsample/draw/DashboardView.kt
@@ -1,9 +1,13 @@
 package com.one.customviewsample.draw
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator.INFINITE
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.BounceInterpolator
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator
 import com.one.ktools.ex.dp
 import kotlin.math.cos
 import kotlin.math.sin
@@ -16,7 +20,7 @@ import kotlin.math.sin
 private const val OPEN_ANGLE = 120F
 private const val MARK = 5
 private val RADIUS = 150.dp
-private val LENGTH = 120.dp
+private val LENGTH = 125.dp
 private val DASH_WIDTH = 2.dp
 private val DASH_LENGTH = 10.dp
 
@@ -27,10 +31,23 @@ class DashboardView(context: Context?, attrs: AttributeSet?) : View(context, att
     private val dash = Path()
     private lateinit var pathEffect: PathEffect
 
+    private var fracation = 0
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+
+    private val animator = ObjectAnimator.ofInt(this, "fracation", 0, 11).setDuration(11 * 1000)
+
+
     init {
         paint.strokeWidth = 3.dp
         paint.style = Paint.Style.STROKE
         dash.addRect(0f, 0f, DASH_WIDTH, DASH_LENGTH, Path.Direction.CCW)
+        animator.interpolator = FastOutLinearInInterpolator()
+        animator.repeatCount= INFINITE
+        animator.start()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -45,7 +62,8 @@ class DashboardView(context: Context?, attrs: AttributeSet?) : View(context, att
         )
 
         val pathMeasure = PathMeasure(path, false)
-        pathEffect = PathDashPathEffect(dash, (pathMeasure.length - DASH_WIDTH) / 20f, 0f, PathDashPathEffect.Style.ROTATE)
+        pathEffect = PathDashPathEffect(dash, (pathMeasure.length - DASH_WIDTH) / 10f, 0f, PathDashPathEffect.Style.ROTATE)
+
 
     }
 
@@ -60,11 +78,11 @@ class DashboardView(context: Context?, attrs: AttributeSet?) : View(context, att
         paint.pathEffect = null
 
         canvas.drawLine(width / 2f, height / 2f,
-                (width / 2f + LENGTH * cos(markToRadians(MARK))).toFloat(),
-                (height / 2f + LENGTH*sin(markToRadians(MARK))).toFloat(), paint)
+                (width / 2f + LENGTH * cos(markToRadians(fracation))).toFloat(),
+                (height / 2f + LENGTH * sin(markToRadians(fracation))).toFloat(), paint)
 
 
     }
 
-    private fun markToRadians(mark: Int) = Math.toRadians((90 + OPEN_ANGLE / 2f + (360 - OPEN_ANGLE) / 20f * mark).toDouble())
+    private fun markToRadians(mark: Int) = Math.toRadians((90 + OPEN_ANGLE / 2f + (360 - OPEN_ANGLE) / 10f * mark).toDouble())
 }
