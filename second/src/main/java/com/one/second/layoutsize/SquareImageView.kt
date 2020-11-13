9f8301e3da818afcd8dcb7f44f9fc78b26560ae6
Kaibin <diaokaibin@gmail.com>
2020-11-13 21:42:30 +0800

    update

---
+++ second/src/main/java/com/one/second/layoutsize/SquareImageView.kt
@@ -0,0 +1,40 @@
+package com.one.second.layoutsize
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageView
+import java.lang.Double.min
+import kotlin.math.min
+
+/**
+ * @author  diaokaibin@gmail.com on 11/13/20.
+ */
+class SquareImageView(context: Context, attrs: AttributeSet?) : androidx.appcompat.widget.AppCompatImageView(context, attrs) {
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+
+        measuredHeight
+        measuredWidth
+        width
+        height
+
+        var size = min(measuredHeight,measuredWidth)
+        setMeasuredDimension(size,size)
+
+
+    }
+
+    override fun layout(l: Int, t: Int, r: Int, b: Int) {
+
+//        var width = r - l
+//        var height = b - t
+//
+//        var size = min(width, height)
+        super.layout(l, t, r, b)
+
+    }
+}
