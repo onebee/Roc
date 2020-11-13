ce54a0546f09e28c158d34ce4a4add680921ec26
Kaibin <diaokaibin@gmail.com>
2020-11-13 22:15:04 +0800

    update

--- second/src/main/java/com/one/second/layoutsize/CircleView.kt
+++ second/src/main/java/com/one/second/layoutsize/CircleView.kt
@@ -2,10 +2,10 @@ package com.one.second.layoutsize
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.graphics.toColorInt
 import com.one.ktools.ex.dp
 
 /**
@@ -17,13 +17,20 @@ val PADDING = 100.dp
 
 class CircleView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
-
     val paint = Paint().apply {
-        color = "#987654".toColorInt()
+        color = Color.YELLOW
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawCircle(PADDING + RADIUS, PADDING + RADIUS, RADIUS, paint)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        var size = ((RADIUS + PADDING) * 2).toInt()
+        setMeasuredDimension(size, size)
+
+
     }
 }
