7a2be68b91171a5e924ae02cfd6568d6b9588a2d
Kaibin <diaokaibin@gmail.com>
2020-11-15 10:02:59 +0800

    update

--- second/src/main/java/com/one/second/MainActivity.kt
+++ second/src/main/java/com/one/second/MainActivity.kt
@@ -1,24 +1,12 @@
 package com.one.second
 
-import android.animation.*
-import android.graphics.PointF
-import android.graphics.drawable.BitmapDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.postDelayed
-import com.one.kcore.extension.dp
-import com.one.second.animate.CameraView2
-import com.one.second.animate.ProvinceTypeEvaluator
-import com.one.second.clipandcamera.CameraView
-import com.one.second.draw.PieView
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.random.Random
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 
---
+++ second/src/main/java/com/one/second/constraintlayout/CircularRevealHelper.kt
@@ -0,0 +1,31 @@
+package com.one.second.constraintlayout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewAnimationUtils
+import androidx.constraintlayout.widget.ConstraintHelper
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.math.hypot
+
+/**
+ * @author  diaokaibin@gmail.com on 11/15/20.
+ */
+class CircularRevealHelper(context: Context?, attrs: AttributeSet?) : ConstraintHelper(context, attrs) {
+
+
+    override fun updatePostLayout(container: ConstraintLayout) {
+        super.updatePostLayout(container)
+
+        referencedIds.forEach {
+
+            val view = container.getViewById(it)
+
+            val radius = hypot(view.width.toDouble(), view.height.toDouble())
+
+            ViewAnimationUtils.createCircularReveal(view, 0, 0, 0f, radius.toFloat())
+                    .setDuration(2000)
+                    .start()
+        }
+
+    }
+}
